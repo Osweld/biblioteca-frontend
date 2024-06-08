64dfd673a79bd9d9f2b2e5737544055b165d9a6e
Osweld <38056431+Osweld@users.noreply.github.com>
2024-06-08 16:12:13 -0600

    se agrego el dashboard

--- src/app/feature/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/feature/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 import { DashboardService } from '../../dashboard.service';
 import { Estadisticas, PrestamosDay } from '../../dashboard.interface';
+import { Chart, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -17,14 +18,48 @@ export default class DashboardComponent {
   public dashboardService = inject(DashboardService);
 
   constructor() {
+
+
+  }
+
+  ngOnInit(): void {
     this.dashboardService.getEstadisticas().subscribe(estadisticas => {
       this.estadisticas = estadisticas;
     });
 
     this.dashboardService.getPrestamosDay().subscribe(prestamosPorDia => {
       this.prestamosPorDia = prestamosPorDia;
+      this.renderPrestamosChart();
     });
 
+    Chart.register(...registerables);
+  }
+
+
+  renderPrestamosChart(): void {
+    if (this.prestamosPorDia) {
+      const ctx = (document.getElementById('prestamosChart') as HTMLCanvasElement).getContext('2d');
+      new Chart(ctx!, {
+        type: 'bar',
+        data: {
+          labels: this.prestamosPorDia.map(d => new Date(d.dia).toLocaleDateString()),
+          datasets: [{
+            label: 'Préstamos por Día',
+            data: this.prestamosPorDia.map(d => d.numeroPrestamos),
+            backgroundColor: 'rgba(54, 162, 235, 0.2)',
+            borderColor: 'rgba(54, 162, 235, 1)',
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
+        }
+      });
+    }
   }
 
 }
