ee9605c7707809fd525147c769c5f54049603877
Osweld <38056431+Osweld@users.noreply.github.com>
2024-06-09 10:20:34 -0600

    mejoras a la landing page

--- src/app/feature/home/pages/home/home.component.ts
+++ src/app/feature/home/pages/home/home.component.ts
@@ -9,5 +9,18 @@ import { RouterModule } from '@angular/router';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  features = [
+    {
+      title: 'Amplia Colección de Libros',
+      description: 'Nuestra biblioteca cuenta con una vasta colección de libros en diversas áreas del conocimiento.'
+    },
+    {
+      title: 'Acceso a Recursos Digitales',
+      description: 'Disponemos de una gran variedad de recursos digitales accesibles desde cualquier lugar.'
+    },
+    {
+      title: 'Ambiente de Estudio Cómodo',
+      description: 'Ofrecemos espacios de estudio tranquilos y cómodos para todos nuestros usuarios.'
+    }
+  ];
 }
